acde6b3365bad35f694f08c54727876108678d38
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2021-04-07 23:32:42 +0300

    Dynamic size calculator
    Variable cell size and aspect ratio

--- game.js
+++ game.js
@@ -1,7 +1,24 @@
 const body = document.querySelector("body");
 let canvas = document.createElement("canvas");
-const width = 384;
-const height = 256;
+
+let aspectRatio = {
+	widescreen1:{name:"16:9", ratio:16/9},
+	standard1:{name:"4:3", ratio:4/3}
+};
+
+let width = 1000;
+
+const cellCountX = 32;
+const cellSize = width/cellCountX - 1;
+width = cellSize * cellCountX;
+
+const cellCountY = cellCountX / 2;
+const height = cellSize * cellCountY;
+
+// const height = width/aspectRatio.widescreen1.ratio / 16 % 0 ?;
+
+
+
 canvas.width = width;
 canvas.height = height;
 canvas.style.border = "1px solid black";
@@ -10,7 +27,7 @@ body.appendChild(canvas);
 canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 16, 16);
+ctx.fillRect(0, 0, cellSize, cellSize);
 
 var dirKey = null;
 var currentPosX = 0;
@@ -44,17 +61,17 @@ function moveRect(dirKey) {
     }
 
     function moveHorizontally() {
-        if (0 < currentPosX && dirKey == "a" || currentPosX + 1 < width / 16 && dirKey == "d") {
-            ctx.clearRect(currentPosX * 16, currentPosY * 16, 16, 16);
-            ctx.fillRect(nextPos * 16, currentPosY * 16, 16, 16);
+        if (0 < currentPosX && dirKey == "a" || currentPosX + 1 < width / cellSize && dirKey == "d") {
+            ctx.clearRect(currentPosX * cellSize, currentPosY * cellSize, cellSize, cellSize);
+            ctx.fillRect(nextPos * cellSize, currentPosY * cellSize, cellSize, cellSize);
             currentPosX = nextPos;
         }
     }
 
     function moveVertically() {
-        if (0 < currentPosY && dirKey == "w" || currentPosY + 1 < height / 16 && dirKey == "s") {
-            ctx.clearRect(currentPosX * 16, currentPosY * 16, 16, 16);
-            ctx.fillRect(currentPosX * 16, nextPos * 16, 16, 16);
+        if (0 < currentPosY && dirKey == "w" || currentPosY + 1 < height / cellSize && dirKey == "s") {
+            ctx.clearRect(currentPosX * cellSize, currentPosY * cellSize, cellSize, cellSize);
+            ctx.fillRect(currentPosX * cellSize, nextPos * cellSize, cellSize, cellSize);
             currentPosY = nextPos;
         }
     }
